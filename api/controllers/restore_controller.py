d6a6533b2c81f2d706386a8d24462ce129de0397
Mdran2112 <dran2112@gmail.com>
2023-03-13 15:47:29 -0300

    ADD - error message if user writes an incorrect table in restore endpoint

--- api/controllers/restore_controller.py
+++ api/controllers/restore_controller.py
@@ -1,15 +1,19 @@
 import json
 import logging
 import time
+from flask_smorest import abort
 
 from flask.views import MethodView
-from flask_api.status import HTTP_200_OK
+from flask_api.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 from flask_smorest import Blueprint
 
 from api import RESTORE_SERVICE
 from api.controllers import auth
+from database.models import EMPLOYEES_TABLE, DEPARTMENT_TABLE, JOB_TABLE
 
 blp = Blueprint("Restore", "restore", description="Restore table from avro backup file.")
+
+TABLES = [EMPLOYEES_TABLE, DEPARTMENT_TABLE, JOB_TABLE]
 
 
 @blp.route("/restore/<string:table>")
@@ -26,7 +30,8 @@ class RestoreController(MethodView):
     def put(self, table: str):
         t0 = time.time()
         logging.info(f"Restoring table {table}...")
-
+        if table not in TABLES:
+            abort(HTTP_400_BAD_REQUEST, message=f"Please write an existing table: {TABLES}")
         res = RESTORE_SERVICE.restore(table=table)
 
         tf = time.time()
